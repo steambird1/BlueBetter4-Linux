ed99e3612a5ceb7a17927386162a3521e47863b4
steambird1 <77623231+steambird1@users.noreply.github.com>
2023-04-30 20:30:48 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -35,29 +35,49 @@ intValue calculate(string expr, varmap &vm);
 void raiseError(intValue raiseValue, varmap &myenv, string source_function = "Unknown source", size_t source_line = 0, double error_id = 0, string error_desc = "");
 intValue getValue(string single_expr, varmap &vm, bool save_quote = false, int multithreading = -1);
 
+#define DWORD int
+#define FOREGROUND_BLUE 0x1
+#define FOREGROUND_GREEN 0x2
+#define FOREGROUND_RED 0x4
+#define FOREGROUND_INTENSITY 0x8
+#define BACKGROUDN_BLUE 0x10
+#define BACKGROUND_GREEN 0x20
+#define BACKGROUND_RED 0x40
+#define BACKGROUND_INTENSITY 0x80
+
 //HANDLE stdouth;
-//DWORD precolor = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN, nowcolor = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
+DWORD precolor = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN, nowcolor = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
 void setColor(int color) {
+	static map<int, int> color_mapping = { {0x4, 1}, {0x2, 2}, {0x6, 3}, {0x1, 4}, {0x5, 5}, {0x3, 6}, {0x7, 7} };
+	// Deal with foreground part:
+	int fore = color & 7;
+	int fore_result = 30, back_result = 40;
+	if (color & 0x8) fore_result = 90;
+	if (color & 0x80) back_result = 100;
+	fore_result += color_mapping[fore];
+	int back = color & 112;
+	back_result += color_mapping[back>>4];
+
 	//SetConsoleTextAttribute(stdouth, color);
-	//precolor = nowcolor;
-	//nowcolor = color;
+	printf("\033[%dm\033[%dm",fore_result,back_result);
+	precolor = nowcolor;
+	nowcolor = color;
 }
 
 inline void begindout() {
-	//setColor(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	setColor(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 }
 
 inline void endout() {
-	
-	//setColor(precolor);
+	setColor(precolor);
 }
 
 inline void specialout() {
-	//setColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	setColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 }
 
 inline void curlout() {
-	//setColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	setColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 }
 
 const int max_indent = 65536;
@@ -3259,7 +3279,7 @@ int main(int argc, char* argv[]) {
 	// End
 
 	if (argc <= 1 && !file.length() && !code.length()) {
-		cout << "Usage: " << argv[0] << " filename [options]";
+		cout << "\033[93mUsage: " << argv[0] << " filename [options]\033[0m" << endl;
 		return 1;
 	}
 	
@@ -3271,7 +3291,7 @@ int main(int argc, char* argv[]) {
 
 	if (file == "--version") {
 		// = true;
-		cout << version_info << endl;
+		cout << "\033[94m" << version_info << "\033[0m" << endl;
 		return 0;
 	}
 
@@ -3346,5 +3366,7 @@ int main(int argc, char* argv[]) {
 		endout();
 	}
 
-	return preRun(code, reqs, callers).numeric;
+	int rescode = preRun(code, reqs, callers).numeric;
+	printf("\033[0m"); // Clean up environment
+	return rescode;
 }
